00c63a44cc6d1d0ef6d20d4f01cda95d6b20ddde
petar <aaabo@hotmail.com>
2022-08-10 10:49:35 +0200

    Dynamic styles with CSS modules

--- 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
-import styled from'styled-components';
-const FormControl =styled.div`
+import styles from './CourseInput.module.css';
+
+/* const FormControl =styled.div`
 
   margin: 0.5rem 0;
 
@@ -33,7 +33,7 @@ const FormControl =styled.div`
 
 
 `;
-
+ */
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
   const [isValid,setIsValid]=useState(true);
@@ -54,10 +54,10 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl invalid={!isValid}>
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
         <label >Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
